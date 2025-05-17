744a3ba762a9b3ecb95378fef0da092a07680371
fmfm2024 <fairymade2024@gmail.com>
2025-05-17 20:48:47 +0100

    .

--- src/app/purchase-quantity/page.tsx
+++ src/app/purchase-quantity/page.tsx
@@ -1,9 +1,18 @@
+import UpdatePurchaseQuantity from "@components/purchase-quantity";
 import { fetchCSRF } from "../fetch-csrf";
+import { httpRequestHeader } from "@misc";
+import { IState } from "@components/purchase-quantity/purchaseQuantitySlice";
 
 const PurchaseQuantityPage = async() => {
     const csrf = await fetchCSRF() || ''
+    
+    const resp = await fetch(`${process.env.FM_CLIENT_ADMIN_API_URL}/admin/purchase-quantity-page-init`,{
+        headers:httpRequestHeader(true,'SSR',true)
+    })
+    const initialState = await resp.json() as IState
+
     return (
-        <></>
+        <UpdatePurchaseQuantity {...{csrf,initialState}} />
     )
 }
 
