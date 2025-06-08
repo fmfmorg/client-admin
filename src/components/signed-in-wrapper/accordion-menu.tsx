625bcb4bb64c9a790e0711e3fd926f15d770bdbe
fmfm2024 <fairymade2024@gmail.com>
2025-06-08 18:57:00 +0100

    .

--- src/components/signed-in-wrapper/accordion-menu.tsx
+++ src/components/signed-in-wrapper/accordion-menu.tsx
@@ -30,7 +30,8 @@ const AccordionMenu = (
                 {menuList.map((item, i) => (
                     <ListItem key={i} disablePadding>
                         <ListItemButton component={Link} href={item.pathname} target={!!newTab ? '_blank' : '_self'}>
-                            <ListItemText primary={item.name} />
+                            {/* <ListItemText primary={item.name} /> */}
+                            <ListItemText primary={item.name + ' ' + JSON.stringify(!!newTab)} />
                         </ListItemButton>
                     </ListItem>
                 ))}
