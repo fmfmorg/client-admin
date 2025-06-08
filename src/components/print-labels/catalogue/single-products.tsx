9d7744e466cd9e460167a467bad0414c3e199b21
fmfm2024 <fairymade2024@gmail.com>
2025-06-08 21:26:31 +0100

    .

--- src/components/print-labels/catalogue/single-products.tsx
+++ src/components/print-labels/catalogue/single-products.tsx
@@ -2,7 +2,50 @@ import { selectSingleProductIDs } from "@components/pricing/selectors"
 import { useAppSelector } from "@store/hooks"
 import ImageList from "@mui/material/ImageList"
 import ImageListItem from "@mui/material/ImageListItem"
-import { IInternalItemSpecification, ISkuMapItem } from "src/interfaces"
+import ImageListItemBar from "@mui/material/ImageListItemBar"
+import { IExternalItem, IInternalItemSpecification, ISkuMapItem } from "src/interfaces"
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+import TextField from "@mui/material/TextField"
+
+const EditQtyField = ({id}:{id:string}) => {
+    const qty = useAppSelector(state => {
+        const qty = (state.productsReducer.externalItems as IExternalItem[]).find(e => e.externalSkuID === id)?.labelQty || 0
+        return !!qty ? qty.toString() : ''
+    })
+    return (
+        <TextField 
+            fullWidth
+            label='Print Quantity'
+            type='number'
+            defaultValue={qty}
+            slotProps={{
+                htmlInput:{step:1,min:0},
+                inputLabel:{sx:{fontWeight:'bold',color:'#fff'}},
+                input:{
+                    sx:{
+                        fontWeight:'bold',
+                        color:'#fff',
+                        textAlign:'center',
+                        '-moz-appearance':'textfield',
+                        '::-webkit-outer-spin-button':{'-webkit-appearance': 'none'},
+                        '::-webkit-inner-spin-button':{'-webkit-appearance': 'none'},
+                    }
+                },
+            }}
+        />
+    )
+}
+
+const ProductField = ({id}:{id:string}) => {
+    const currentPrice = useAppSelector(state => ((state.productsReducer.externalItems as IExternalItem[]).find(e => e.externalSkuID === id)?.price || 0).toFixed(2))
+    return (
+        <Stack direction='column'>
+            <Typography variant='body2'>{id} - £{currentPrice}</Typography>
+            <EditQtyField {...{id}} />
+        </Stack>
+    )
+}
 
 const SingleProduct = ({id}:{id:string})=> {
     const imgSrc = useAppSelector(state => {
@@ -24,6 +67,7 @@ const SingleProduct = ({id}:{id:string})=> {
                     style={{objectFit:'cover',objectPosition:'center',width:'100%',height:'100%'}}
                 />
             </a>
+            <ImageListItemBar title={<ProductField {...{id}} />} />
         </ImageListItem>
     )
 }
