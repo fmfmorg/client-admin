bac6f9b579de96ae5f970334ff2b14a4862f9635
fmfm2024 <fairymade2024@gmail.com>
2025-06-10 19:27:07 +0100

    .

--- src/components/product-info-external/content.tsx
+++ src/components/product-info-external/content.tsx
@@ -1,16 +1,33 @@
 import Stack from "@mui/material/Stack"
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import { useAppSelector } from "@store/hooks";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { externalIdSelector } from "./selectors";
+import { toggleEditDialog } from "@slices/products";
+import { useEffect } from "react";
 
 const Content = () => {
+    const dispatch = useAppDispatch()
     const options = useAppSelector(externalIdSelector)
+    const value = useAppSelector(state => state.productsReducer.editItemID || '')
+    const onChange = (_:any, v:string|string[]|null) => dispatch(toggleEditDialog(typeof v === 'string' ? v : ''))
+
+    const ev = (e:Event) => console.log(e)
+
+    useEffect(()=>{
+        window.addEventListener('keypress',ev)
+        return () => {
+            window.removeEventListener('keypress',ev)
+        }
+    },[])
+
     return (
         <Stack direction='column' rowGap={2} p={2}>
             <Autocomplete 
                 options={options}
                 renderInput={(params) => <TextField {...params} label="SKU #" />}
+                value={value}
+                onChange={onChange}
             />
         </Stack>
     )
--- src/components/product-info-external/selectors.ts
+++ src/components/product-info-external/selectors.ts
@@ -4,4 +4,4 @@ import { IExternalItem } from "src/interfaces";
 
 const state = (state:RootState) => state
 
-export const externalIdSelector = createSelector([state],state=> (state.productsReducer.externalItems as IExternalItem[]).map(e => e.externalSkuID).sort())
+export const externalIdSelector = createSelector([state],state=> (['',(state.productsReducer.externalItems as IExternalItem[]).map(e => e.externalSkuID).sort()]))
