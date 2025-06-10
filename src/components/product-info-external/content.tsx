af1ddf03de46abb665c8f68713203903957488f2
fmfm2024 <fairymade2024@gmail.com>
2025-06-10 19:43:21 +0100

    .

--- src/components/product-info-external/content.tsx
+++ src/components/product-info-external/content.tsx
@@ -4,20 +4,25 @@ import Autocomplete from '@mui/material/Autocomplete';
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { externalIdSelector } from "./selectors";
 import { toggleEditDialog } from "@slices/products";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const Content = () => {
+    // const inputValue = useRef('')
     const dispatch = useAppDispatch()
     const options = useAppSelector(externalIdSelector)
     const value = useAppSelector(state => state.productsReducer.editItemID || '')
     const onChange = (_:any, v:string|string[]|null) => dispatch(toggleEditDialog(typeof v === 'string' ? v : ''))
 
-    const ev = (e:Event) => console.log(e)
+    const ev = (e:Event) => {
+        console.log(e)
+        // if (!['BODY','body'].includes((e.target as HTMLElement).nodeName)) return
+        // if (e.key )
+    }
 
     useEffect(()=>{
-        window.addEventListener('keypress',ev)
+        window.addEventListener('keydown',ev)
         return () => {
-            window.removeEventListener('keypress',ev)
+            window.removeEventListener('keydown',ev)
         }
     },[])
 
