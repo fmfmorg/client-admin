1eef24b4c181b54e7bcd1fc0999d9974806b3366
fmfm2024 <fairymade2024@gmail.com>
2025-07-25 22:58:11 +0100

    .

--- src/components/new-purchase-item/master-form.tsx
+++ src/components/new-purchase-item/master-form.tsx
@@ -2,6 +2,8 @@ import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Stack from "@mui/material/Stack";
 import { ChangeEvent, useState } from "react";
+import NewItemForm from "./new-item-form";
+import OldItemForm from "./old-item-form";
 
 const MasterForm = (
     {
@@ -23,6 +25,7 @@ const MasterForm = (
         <Stack direction='row' justifyContent='center'>
             <Stack direction='column' sx={{maxWidth:'800px'}}>
                 <FormControlLabel control={<Checkbox onChange={newItemCheckboxOnChange} checked={newItem} />} label='New Item' />
+                {newItem ? <NewItemForm {...{external,internal,updateExternal,updateInternal}} /> : <OldItemForm />}
             </Stack>
         </Stack>
     )
