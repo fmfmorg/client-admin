f9de5677305b0ccb1410167dbc9b3b1eae88da15
fmfm2024 <fairymade2024@gmail.com>
2026-06-26 19:43:35 +0100

    260626

--- src/app/new-purchase-item/page.tsx
+++ src/app/new-purchase-item/page.tsx
@@ -7,24 +7,29 @@ import { IProductSupplierItem } from "src/interfaces"
 const NewPurchaseItemPage = async() => {
     const csrf = await fetchCSRF() || ''
 
-    const [productIdResp, resp] = await Promise.all([
-        fetch(`${process.env.FM_CLIENT_ADMIN_API_URL}/admin/new-purchase-item-init`,{
-            headers:httpRequestHeader(true,'SSR',true),
-            cache:'no-store',
-        }),
-        fetch(`${process.env.FM_CLIENT_ADMIN_API_URL}/admin/purchase-quantity-page-init`,{
-            headers:httpRequestHeader(true,'SSR',true),
-            cache:'no-store',
-        })
-    ])
-    const {_external,_internal,_productSupplierItems} = await productIdResp.json() as {
-        _external:string;
-        _internal:string;
-        _productSupplierItems:IProductSupplierItem[];
-    }
-    const initialState = await resp.json() as IState
+    try {
+        const [productIdResp, resp] = await Promise.all([
+            fetch(`${process.env.FM_CLIENT_ADMIN_API_URL}/admin/new-purchase-item-init`,{
+                headers:httpRequestHeader(true,'SSR',true),
+                cache:'no-store',
+            }),
+            fetch(`${process.env.FM_CLIENT_ADMIN_API_URL}/admin/purchase-quantity-page-init`,{
+                headers:httpRequestHeader(true,'SSR',true),
+                cache:'no-store',
+            })
+        ])
+        console.log(resp.status)
+        const {_external,_internal,_productSupplierItems} = await productIdResp.json() as {
+            _external:string;
+            _internal:string;
+            _productSupplierItems:IProductSupplierItem[];
+        }
+        const initialState = await resp.json() as IState
 
-    return <NewPurchaseItem {...{csrf,_internal,_external,initialState,_productSupplierItems}} />
+        return <NewPurchaseItem {...{csrf,_internal,_external,initialState,_productSupplierItems}} />
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 export default NewPurchaseItemPage
